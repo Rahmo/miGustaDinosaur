ff519060fb853d14dedd99a5c201d35e3a1b9ea7
rahmo Alzahrani <abdulrahman.alzahrani@morningstar.com>
2019-04-13 22:01:35 -0500

    done tables

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-_setupActionMenu();
+let myjson;
 
+_connectionSetup();
+_setupActionMenu();
+_setupTableRecords();
 
 let isOpen = false;
-$('.closed-toggle').click(function() {
-    if(isOpen == false) {
-        $('#scene').removeClass("normal");
-        $('#scene').addClass("grow");
-        $('.closed-toggle').addClass("open-toggle");
-        $(".menu-icon").attr("src","assets/icons/arrow-right.png");
-        isOpen = true;
-    } else {
-        $('#scene').removeClass("grow");
-        $('#scene').addClass("normal");
-        $(".closed-toggle").removeClass('open-toggle');
-        $(".menu-icon").attr("src","assets/icons/arrow-left.png");
-        isOpen = false;
-    }
-});
+
+function _connectionSetup() {
+    $.ajax({
+        async: false,
+        url: 'https://raw.githubusercontent.com/Rahmo/miGustaDinosaur/master/SpringCMUIExercise.json',
+        dataType: "json",
+        success: function (data) {
+            myjson = data
+        }
+    });
+}
 
 function _setupActionMenu() {
-    $.getJSON("https://raw.githubusercontent.com/Rahmo/miGustaDinosaur/master/SpringCMUIExercise.json", function(json) {
+    $.getJSON("https://raw.githubusercontent.com/Rahmo/miGustaDinosaur/master/SpringCMUIExercise.json", function (json) {
         console.log();
-        json['Actions'].forEach( (item)=>{
-                let iconPath = `assets/icons/${item.Name.toLowerCase().replace(' ','')}.png`;
+        json['Actions'].forEach((item) => {
+                let iconPath = `assets/icons/${item.Name.toLowerCase().replace(' ', '')}.png`;
                 let htmlLink = '';
-                if (item.Name == 'Upload'){
-                    htmlLink = '<a class="post"><img class="icon" onclick="postHandler()" src= ' +iconPath + '></a>'
-                }else {
-                    htmlLink = '<a><img class="icon " src= ' +iconPath + '></a>'
+                if (item.Name == 'Upload') {
+                    htmlLink = '<a class="post"><img class="icon" onclick="postHandler()" src= ' + iconPath + '></a>'
+                } else {
+                    htmlLink = '<a><img class="icon " src= ' + iconPath + '></a>'
                 }
                 $('#scene').append(htmlLink)
             }
@@ -35,12 +34,81 @@ function _setupActionMenu() {
     });
 }
 
-let postHandler = function() {
-    var favorite = [];
+function _setupTableRecords() {
+    myjson['GridData'].forEach((item) => {
+            let iconPath;
+            if (item['Type'] == 'Folder') {
+                iconPath = `assets/icons/folder.png`;
+            } else {
+                iconPath = `assets/icons/document.png`;
+            }
+            var t = `<tr data-tag = '${item['Name']}' class="row"> <td> <img class="small-icon " src= ${iconPath}>  <span class="name">${item['Name']}</span></td>\n` +
+                `        <td>${item['Description']}</td>\n` +
+                `        <td>${item['ModifiedDate']}</td>\n` +
+                `        <td></td>\n` +
+                `        <td><input name=\'selection\' type="checkbox" value="${item['Name']}" /></td></tr>`;
+
+            $('tbody').append(t);
+        }
+    )
+}
+
+/* Events */
+$('.closed-toggle').click(function () {
+    if (isOpen == false) {
+        $('#scene').removeClass("normal");
+        $('#scene').addClass("grow");
+        $('.closed-toggle').addClass("open-toggle");
+        $(".menu-icon").attr("src", "assets/icons/arrow-right.png");
+        isOpen = true;
+    } else {
+        $('#scene').removeClass("grow");
+        $('#scene').addClass("normal");
+        $(".closed-toggle").removeClass('open-toggle');
+        $(".menu-icon").attr("src", "assets/icons/arrow-left.png");
+        isOpen = false;
+    }
+});
+
+let postHandler = function () {
+    var selected = [];
     console.log('clickkkk');
     $.each($("input[name='selection']:checked"), function () {
-        favorite.push($(this).val());
+        selected.push($(this).val());
     });
 
-    alert("My favourite sports are: " + favorite.join(", "));
+    alert("You have selected: " + selected.join(", "));
 };
+
+$(document).on('click', '.row', function (e) {
+    let type = e.currentTarget.attributes.getNamedItem('data-tag').value;
+    childrenArray = myjson['GridData'].filter(item => item.Name == type)[0]['Children'];
+    if (childrenArray.length == 0){
+        alert('this item has no sub items');
+    }
+    let isOpen = (e.currentTarget.className.includes('open-row'));
+    if (isOpen) {
+        $('.child-row').remove();
+        e.currentTarget.className = 'row';
+    } else {
+        e.currentTarget.className = 'row open-row';
+        let childTag = '<tr data-tag = \'ReadMe.md\' class="child-row">\n' +
+            '        <td>any Name</td>\n' +
+            '        <td>desc</td>\n' +
+            '        <td></td>\n' +
+            '        <td></td>\n' +
+            '        <td><input name=\'selection\' type="checkbox" value="running" /></td>\n' +
+            '    </tr>';
+        console.log(childrenArray);
+        childrenArray.forEach(item => {
+            var t = `<tr data-tag = '${item['Name']}' class="child-row"> <td> <span >${item['Name']}</span></td>\n` +
+                `        <td>${item['Description']}</td>\n` +
+                `        <td>${item['ModifiedDate']}</td>\n` +
+                `        <td></td>\n` +
+                `        <td><input name=\'selection\' type="checkbox" value="${item['Name']}" /></td></tr>`;
+
+            console.log(t);
+            $(t).insertAfter($(this).closest('tr'));
+        });
+    }
+});
